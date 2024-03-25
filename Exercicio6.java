5941fe91880889faf6254faa64a1cc708bc4dbcf
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:43:37 -0300

    Add files via upload

---
+++ Exercicio6.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+    public static void executar() {
+    System.out.print("Digite a temperatura em Celsius: ");
+    double Celsius = scanner.nextDouble();
+    double Fahrenheit = (9 * Celsius + 160) / 5;
+    System.out.println("Temperatura em Fahrenheit: " + Fahrenheit);
