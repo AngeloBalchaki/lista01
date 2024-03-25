5c04f9b054283cca89c72b517fef55d1e9371763
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:45:57 -0300

    Add files via upload

---
+++ Exercicio15.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+    public static void executar() {
+
+         double calcularDistancia(double x1, double y1, double x2, double y2) {
+            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        }
+    }
