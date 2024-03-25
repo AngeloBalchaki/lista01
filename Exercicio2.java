ecc2ab6c0688d71afdeab0b23cba2c803cc3a7b6
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:42:30 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,12 @@
+pimport java.util.Scanner;
+
+public class Exercicio2 {
+
+    public static void executar() {
+        
+        System.out.print("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+        int soma = num1 + num2;
+        System.out.println("A soma dos números é: " + soma);
