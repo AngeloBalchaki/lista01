521b0ae1dffd3174721c6507b78b54cb33ce02bf
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:43:51 -0300

    Add files via upload

---
+++ Exercicio7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+
+    public static void executar() {
+    System.out.print("Digite um número: ");
+    int num = scanner.nextInt();
+    if (num >= 100 && num <= 200) {
+        System.out.println("O número está no intervalo entre 100 e 200.");
+    } else {
+        System.out.println("O número está fora do intervalo entre 100 e 200.");
+    }
