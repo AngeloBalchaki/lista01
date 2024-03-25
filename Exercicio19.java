93d70ac660abaa04975fbef55a27922db9e9e293
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:46:54 -0300

    Add files via upload

---
+++ Exercicio19.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio19 {
+
+    public static void executar() {Volume do Cilindro
+   
+        System.out.print("Digite o raio do cilindro: ");
+        double raio = scanner.nextDouble();
+        System.out.print("Digite a altura do cilindro: ");
+        double altura = scanner.nextDouble();
+        double volume = 3.14 * raio * raio * altura;
+        System.out.println("Volume do Cilindro: " + volume);
