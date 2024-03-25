1dd62871cf62a95394284b1da5b0c3a69f16d8d1
Angelo Balchaki <164347105+AngeloBalchaki@users.noreply.github.com>
2024-03-25 10:44:02 -0300

    Add files via upload

---
+++ Exercicio8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+
+    public static void executar() {
+    System.out.print("Digite um número: ");
+    int num2 = scanner.nextInt();
+    if (num2 >= 50) {
+        System.out.println("O número é maior ou igual a 50.");
+    } else {
+        System.out.println("O número é menor que 50.");
+    }
